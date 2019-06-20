ce5936ff00c4af068b852e1c52d32cb2ad6be072
Jeremias W <jeremias.wiedmann@hm.edu>
2019-06-20 17:52:24 +0200

    Set up settings for put them into drawer layout.

--- android/app/src/main/java/com/meteocool/SplashActivity.kt
+++ android/app/src/main/java/com/meteocool/SplashActivity.kt
@@ -4,18 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
-import com.meteocool.settings.SettingsActivity
 
 class SplashActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-            startActivity(Intent(this.applicationContext, SettingsActivity::class.java))
-//        if(!isOnboardingCompleted()) {
-//            startActivity(Intent(this.applicationContext, OnboardingActivity::class.java))
-//        }else {
-//            startActivity(Intent(this.applicationContext, MeteocoolActivity::class.java))
-//        }
+        if(!isOnboardingCompleted()) {
+            startActivity(Intent(this.applicationContext, OnboardingActivity::class.java))
+        }else {
+            startActivity(Intent(this.applicationContext, MeteocoolActivity::class.java))
+        }
         finish()
     }
 
